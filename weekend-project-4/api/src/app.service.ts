0b5e3661227ab57eb04208c5c8358ee975cc45eb
Davy Jones <hello@galleon.community>
2024-08-30 09:55:43 +0100

    check minter role working

--- weekend-project-4/api/src/app.service.ts
+++ weekend-project-4/api/src/app.service.ts
@@ -4,6 +4,7 @@ import { createPublicClient, createWalletClient, http } from 'viem';
 import { privateKeyToAccount } from 'viem/accounts';
 import { sepolia } from 'viem/chains';
 import * as tokenJson from './assets/MyToken.json';
+import { getAddress } from 'viem';
 
 @Injectable()
 export class AppService {
@@ -28,27 +29,27 @@ export class AppService {
   }
 
   async getContractAddress(): Promise<string> {
-    return this.configService.get<string>('TOKEN_ADDRESS');
+    return getAddress(await this.configService.get<string>('TOKEN_ADDRESS'));
   }
 
   async checkMinterRole(address: string): Promise<boolean> {
     const MINTER_ROLE = await this.publicClient.readContract({
-      address: this.getContractAddress(),
+      address: await this.getContractAddress(),
       abi: tokenJson.abi,
       functionName: 'MINTER_ROLE',
     });
     const hasRole = await this.publicClient.readContract({
-      address: this.getContractAddress(),
+      address: await this.getContractAddress(),
       abi: tokenJson.abi,
       functionName: 'hasRole',
-      args: [MINTER_ROLE, address],
+      args: [MINTER_ROLE, getAddress(address)],
     });
     return hasRole;
   }
 
   async getTotalSupply() {
     return await this.publicClient.readContract({
-      address: this.getContractAddress(),
+      address: await this.getContractAddress(),
       abi: tokenJson.abi,
       functionName: 'totalSupply',
     });
@@ -56,10 +57,10 @@ export class AppService {
 
   async mintTokens(address: string, amount: number) {
     return await this.walletClient.writeContract({
-      address: this.getContractAddress(),
+      address: await this.getContractAddress(),
       abi: tokenJson.abi,
       functionName: 'mint',
-      args: [address, amount],
+      args: [getAddress(address), amount],
     });
   }
 
@@ -69,10 +70,10 @@ export class AppService {
 
   async getTokenBalance(address: string) {
     return await this.publicClient.readContract({
-      address: this.getContractAddress(),
+      address: await this.getContractAddress(),
       abi: tokenJson.abi,
       functionName: 'balanceOf',
-      args: [address],
+      args: [getAddress(address)],
     });
   }
 }
